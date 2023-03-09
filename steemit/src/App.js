bfb80b20845a559fca3305322c83b697d7e1651e
Babar Ali <alibabar3699@gmail.com>
2023-03-09 14:30:45 +0500

    first commit to test

---
+++ steemit/src/App.js
@@ -0,0 +1,11 @@
+import './assets/css/style.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>Some text</p>
+    </div>
+  );
+}
+
+export default App;
