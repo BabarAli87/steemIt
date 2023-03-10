16f2a5fb464fda8653e119582e4719ad7b45ea85
Babar ali <alibabar3699@gmail.com>
2023-03-10 05:35:53 -0800

    office flat merge

--- src/Navbar.js
+++ src/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar() {
 
 
   return (
-    <header className="bg-white">
+    <header className="bg-gray-800">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
@@ -43,7 +43,7 @@ export default function Navbar() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-300"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -52,9 +52,9 @@ export default function Navbar() {
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative border-transparent focus:border-transparent  focus:ring-0">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-300">
               Explore
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-300" aria-hidden="true" />
             </Popover.Button>
 
             <Transition
